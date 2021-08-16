18f990fc34123e549c29b7cdca49545c0bbfb3d6
Asavari Ambavane <ambavaneasavari@gmail.com>
2021-08-16 12:52:32 +0530

    health stats page done.

--- client/src/views/HealthStats/HealthStats.js
+++ client/src/views/HealthStats/HealthStats.js
@@ -1,73 +1,117 @@
 import React, { useState } from 'react'
 import { malaria } from '../../info/malaria'
 import { tuberculosis } from '../../info/tuberculosis'
+import { lifeExpectancy } from '../../info/lifeExpectancyAtBirth';
+import { maternalMortality } from '../../info/maternalMortality';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import GridContainer from "../../components/Grid/GridContainer";
 import GridItem from '../../components/Grid/GridItem';
 import Chart from 'react-google-charts';
+import { Typography } from '@material-ui/core';
 
 const HealthStats = () => {
 
     const [countryMalaria, setCountryMalaria] = useState('Afghanistan')
     const [countryTB, setCountryTB] = useState('Afghanistan')
+    const [countryLifeExp, setCountryLifeExp] = useState('Afghanistan')
+    const [countryMatMor, setCountryMatMor] = useState('Afghanistan')
 
-    console.log(malaria.filter(x => x["country"] === countryMalaria)[0]["values"])
-
-    const SelectMenu = ({ countryList, country, setCountry }) => {
+    const SelectMenu = ({ countryList, country, setCountry, text }) => {
         return (
-            <div style={{display: 'flex', alignItems: 'center'}}>
-            <label style={{marginRight: '10px'}}>Country</label>
-            <Select size="small" variant='outlined' value={country} onChange={(e) => setCountry(e.target.value)}>
-                {
-                    countryList.map((elem) => {
-                        return (
-                            <MenuItem value={elem["country"]}>{elem["country"]}</MenuItem>
-                        )
-                    })
-                }
-            </Select>
-            </div>
+            <>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+                    <Typography variant="h5" gutterBottom>
+                        {text}
+                    </Typography>
+                    <Select size="small" variant='outlined' value={country} onChange={(e) => setCountry(e.target.value)}>
+                        {
+                            countryList.map((elem) => {
+                                return (
+                                    <MenuItem value={elem["country"]}>{elem["country"]}</MenuItem>
+                                )
+                            })
+                        }
+                    </Select>
+                </div>
+            </>
         )
     }
 
     return (
-        <div>
+        <div className='heath-stats-container'>
             <GridContainer>
-                <GridItem xs={12} sm={1}></GridItem>
-                <GridItem xs={12} sm={5}>
+                <GridItem xs={12} sm={12} md={6}>
                     <div style={{ margin: '10px 0' }}>
-                        <SelectMenu countryList={malaria} country={countryMalaria} setCountry={setCountryMalaria} />
+                        <SelectMenu text='Incedence of Malaria' countryList={malaria} country={countryMalaria} setCountry={setCountryMalaria} />
                         <Chart
                             height={"300px"}
                             chartType="AreaChart"
                             loader={<div>Loading Chart...</div>}
                             data={[
-                                ["Year", "Value"],
+                                ["Year", "Malaria incidence (per 1000 population at risk)"],
                                 ...malaria.filter(x => x["country"] === countryMalaria)[0]["values"]
                             ]}
                             options={{
                                 title: `Incedence of Malaria in ${countryMalaria} over the years.`,
-                                vAxis: { logScale: true }
+                                vAxis: { logScale: false }
                             }}
                             legendToggle
                         />
                     </div>
                 </GridItem>
-                <GridItem xs={12} sm={5}>
+                <GridItem xs={12} sm={12} md={6}>
                     <div style={{ margin: '10px 0' }}>
-                        <SelectMenu countryList={tuberculosis} country={countryTB} setCountry={setCountryTB} />
+                        <SelectMenu text='Incedence of Tuberculosis' countryList={tuberculosis} country={countryTB} setCountry={setCountryTB} />
                         <Chart
                             height={"300px"}
                             chartType="AreaChart"
                             loader={<div>Loading Chart...</div>}
                             data={[
-                                ["Year", "Value"],
+                                ["Year", "Incidence of tuberculosis (per 100,000 population per year)"],
                                 ...tuberculosis.filter(x => x["country"] === countryTB)[0]["values"]
                             ]}
                             options={{
                                 title: `Incedence of Tuberculosis in ${countryTB} over the years.`,
-                                vAxis: { logScale: true }
+                                vAxis: { logScale: false }
+                            }}
+                            legendToggle
+                        />
+                    </div>
+                </GridItem>
+                <GridItem xs={12} sm={12} md={6}>
+                    <div style={{ margin: '3rem 0' }}>
+                        <SelectMenu text='Maternal Mortality Rate' countryList={maternalMortality} country={countryMatMor} setCountry={setCountryMatMor} />
+                        <Chart
+                            height={"300px"}
+                            chartType="AreaChart"
+                            loader={<div>Loading Chart...</div>}
+                            data={[
+                                ["Year", "Maternal mortality ratio"],
+                                ...maternalMortality.filter(x => x["country"] === countryMatMor)[0]["values"]
+                            ]}
+                            options={{
+                                title: `Maternal mortality ratio (per 100 000 live births) in ${countryMatMor}`,
+                                vAxis: { logScale: false }
+                            }}
+                            legendToggle
+                        />
+                    </div>
+                </GridItem>
+                <GridItem xs={12} sm={12} md={6}>
+                    <div style={{ margin: '3rem 0' }}>
+                        <SelectMenu text='Life Expectancy (years)' countryList={lifeExpectancy} country={countryLifeExp} setCountry={setCountryLifeExp} />
+                        <Chart
+                            height={"300px"}
+                            chartType="ColumnChart"
+                            loader={<div>Loading Chart...</div>}
+                            data={[
+                                ["Year", "Males", "Females"],
+                                ...lifeExpectancy.filter(x => x["country"] === countryLifeExp)[0]["values"]
+                            ]}
+                            options={{
+                                title: `Life expectancy at birth (years) in ${countryLifeExp} over the years.`,
+                                vAxis: { logScale: false }
                             }}
                             legendToggle
                         />
